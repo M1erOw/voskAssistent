637d3d9291a0f57ad9bb2a337863a4cfc253e18c
Vitaly Kovganov <dbnfkbqrrr@gmail.com>
2026-03-17 15:10:58 +0300

    update README

--- triggerNew.py
+++ triggerNew.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import argparse
+from multiprocessing import Process
 import queue
 import sys
 import json
 import time
+import pyautogui
 import sounddevice as sd
 import webbrowser
 from vosk import Model, KaldiRecognizer, SetLogLevel
@@ -24,25 +26,6 @@ def callback(indata, frames, time, status):
     if status:
         print(status, file=sys.stderr)
     q.put(bytes(indata))
-
-def proccessCommand(com):
-    if (not com):
-        # wake()           # показ окна с текстом + функциями
-        return
-    command = com.split(maxsplit = 1)
-    act = command[0]
-    obj = command[1]
-
-    ###############  Определить команды из 1-го слова и 2-ух и больше ###################
-
-    if (checkSimilar(act,"введи") > 0.8): # match/case?
-        print(obj)
-    elif (checkSimilar(act + " " + obj.split()[0], "открой браузер") > 0.8):
-        webbrowser.open("https://www.google.com/", new=2)
-    elif (checkSimilar(act, "найди") > 0.8):
-        webbrowser.open("https://www.google.com/search?q=" + obj.replace(' ','+'), new=2)
-    else:
-        print("Неизвестная команда")
 
 def checkSimilar(action,template):
     S = (1 << len(action)) - 1
@@ -65,127 +48,200 @@ def checkSimilar(action,template):
     norm_sim = 1.0 - norm_dist
     return norm_sim
 
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument(
-    "-l", "--list-devices", action="store_true",
-    help="show list of audio devices and exit")
-args, remaining = parser.parse_known_args()
-if args.list_devices:
-    print(sd.query_devices())
-    parser.exit(0)
+def findMostSimilar(words):
+    actions = { 3 : ["сверни все окна", "запиши в файл"],
+                2 : ["сделай скриншот", "создай файл", "открой браузер", "найди документ", "создай напоминание"],
+                1 : ["напиши", "найди"]}
+    for numWords in actions:
+        for action in actions[numWords]:
+            if checkSimilar(" ".join(words[:numWords]),action) > 0.8:
+                return action
+    return None
 
-parser = argparse.ArgumentParser(
-    description=__doc__,
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    parents=[parser])
-parser.add_argument(
-    "-d", "--device", type=int_or_str,
-    help="input device (numeric ID or substring)")
-parser.add_argument(
-    "-r", "--samplerate", type=int, help="sampling rate")
-parser.add_argument(
-    "-t", "--trigger", type=str, default="мормышка",
-    help="trigger word or phrase to activate (default: 'мормышка')")
-parser.add_argument(
-    "-et", "--endTrig", type=str, default=None,
-    help="trigger word or phrase to do task (by default use 5 seconds timer)")
-args = parser.parse_args(remaining)
+mapping = {'один': 1, 'два': 2, 'три': 3, 'четыре': 4, 'пять': 5, 'шесть': 6, 'семь': 7, 'восемь': 8, 'девять': 9, 'десять': 10, 'одиннадцать': 11,
+               'двенадцать': 12, 'тринадцать': 13, 'четырнадцать': 14, 'пятнадцать': 15, 'шестнадцать': 16, 'семнадцать': 17, 'восемнадцать': 18, 'девятнадцать': 19,
+               'двадцать': 20, 'тридцать': 30, 'сорок': 40, 'пятьдесят': 50, 'шестьдесят': 60, 'семдесят': 70, 'восемдесят': 80, 'девяносто': 90 ,'сто': 100}
 
-try:
-    if args.samplerate is None:
-        device_info = sd.query_devices(args.device, "input")
-        args.samplerate = int(device_info["default_samplerate"])
 
-    model = Model(model_name="vosk-model-small-ru-0.22")
+def createReminder(timeAndText):
+    vremes = 0
+    for i, word in enumerate(timeAndText):
+        if word in mapping:
+            vremes += mapping[word]
+        else:
+            break
+    text = timeAndText[i:]
+    time.sleep(vremes)
+    pyautogui.alert(text=text, title='Reminder', button='OK')
 
-    with sd.RawInputStream(samplerate=args.samplerate, blocksize=8000, device=args.device,
-                           dtype="int16", channels=1, callback=callback):
-        print("#" * 80)
-        print("Press Ctrl+C to stop the recording")
-        print(f"Trigger word: '{args.trigger}'")
-        if args.endTrig is not None:
-            print(f"End trigger word: '{args.endTrig}'")
-        print("#" * 80)
+def processCommand(words):
+    if (not words):
+        # wake()           # показ окна с текстом + функциями
+        return
 
-        rec = KaldiRecognizer(model, args.samplerate)
-        rec.SetWords(True)
-        rec.SetPartialWords(True)
+    match findMostSimilar(words[:3]):
+        case "сверни все окна":
+            pyautogui.hotkey('win', 'd')
+        # case "запиши в файл":
+        #     if len(words) > 3:
+        #         with open(words[3] + '.txt','w') as file:
+        #             file.write(" ".join(words[4:]))
+        case "сделай скриншот":
+            screenshot = pyautogui.screenshot()
+            screenshot.save('screenshot.png')
+        case "создай файл":
+            if len(words) > 2:
+                with open(words[2] + '.txt','w'):
+                    pass
+        case "открой браузер":
+            webbrowser.open("https://www.google.com/", new=2)
+        # case "найди документ":
+        #     pass
+        # case "создай напоминание":
+        #     proc = Process(target=createReminder,args=(words[2:],))
+        #     proc.start()
+        case "напиши":
+            print(" ".join(words[1:]))
+        case "найди":
+            webbrowser.open("https://www.google.com/search?q=" + " ".join(words[1:]), new=2)
+        case _:
+            print("Неизвестная команда")
 
-        command = ""
-        commandIsReady = False
-        timer = 5.0
-        proccessComm = False
+if __name__ == '__main__':
 
-        # start = time.perf_counter()
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument(
+        "-l", "--list-devices", action="store_true",
+        help="show list of audio devices and exit")
+    args, remaining = parser.parse_known_args()
+    if args.list_devices:
+        print(sd.query_devices())
+        parser.exit(0)
 
-        while True:
-            data = q.get()
-                        
-            if rec.AcceptWaveform(data):
-                # end = time.perf_counter()
-                # print(f"{end-start:.6f}")
-                # start = end
-                # print(rec.Result())
-                # cprint("###########################")
-                result_json = rec.Result()
-                result = json.loads(result_json)
-                text = result.get('text')
-                if text:
-                    idx = -1
-                    if (not proccessComm):
-                        try:
-                            idx = text.split().index(args.trigger)
-                            proccessComm = True
-                            # print(idx)
-                        except ValueError:
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        parents=[parser])
+    parser.add_argument(
+        "-d", "--device", type=int_or_str,
+        help="input device (numeric ID or substring)")
+    parser.add_argument(
+        "-r", "--samplerate", type=int, help="sampling rate")
+    parser.add_argument(
+        "-t", "--trigger", type=str, default="мормышка",
+        help="trigger word or phrase to activate (default: 'мормышка')")
+    # parser.add_argument(
+    #     "-et", "--endTrig", type=str, default=None,
+    #     help="trigger word or phrase to do task (by default use 5 seconds timer)")
+    args = parser.parse_args(remaining)
+
+    try:
+        if args.samplerate is None:
+            device_info = sd.query_devices(args.device, "input")
+            args.samplerate = int(device_info["default_samplerate"])
+
+        model = Model(model_name="vosk-model-small-ru-0.22")
+
+        with sd.RawInputStream(samplerate=args.samplerate, blocksize=8000, device=args.device,
+                            dtype="int16", channels=1, callback=callback):
+            print("#" * 80)
+            print("Press Ctrl+C to stop the recording")
+            print(f"Trigger word: '{args.trigger}'")
+            # if args.endTrig is not None:
+            #     print(f"End trigger word: '{args.endTrig}'")
+            print("#" * 80)
+
+            rec = KaldiRecognizer(model, args.samplerate)
+            rec.SetWords(True)
+            rec.SetPartialWords(True)
+
+            command_words = []
+            silence = 2.0
+            listening = False
+            last_word_time = None
+            last_result_time = time.perf_counter()
+
+            while True:
+                data = q.get()
+                            
+                if rec.AcceptWaveform(data):
+                    result = json.loads(rec.Result())
+                    text = result.get('text','')
+                    words = result.get('result',[])
+                    if not words: # случай тишины(каждые ~5 секунд тишины rec.Result() возвращает пустую строку)
+                        print("тишь да гладь")
+                        #################
+                        # if listening:
+                        #     # По идее сюда никогда не попадем Значит можно убрать
+                        #     command = " ".join(command_words)
+                        #     print("Команда 1: ",command)
+                        #     processCommand(command_words)
+                        #     listening = False
+                        #     command_words = []
+                        #     last_word_time = None
+                        #################    
+                        continue
+                    i = 0
+                    while i < len(words):
+                        word = words[i]['word']
+                        start = words[i]['start']
+                        end = words[i]['end']
+
+                        if (not listening):
+                            if word == args.trigger:
+                                listening = True
+                                command_words = []
+                                last_word_time = end
+                            i += 1
                             continue
-                    if (args.endTrig is None):
-                        data = result.get('result','')
-                        # print(data)
-                        if data:
-                            for i in range(idx + 1,len(data)):
-                                wordLen = data[i]['end'] - data[i]['start']
-                                timer -= wordLen
-                                if (timer  > 0):
-                                    command += " " + data[i]['word']
-                                else:
-                                    break
-                            if timer > 0:
-                                pass
-                            else:
-                                print(f"Команда: {command}")
-                                proccessCommand(command)
-                                proccessComm = False
-                                timer = 5.0
-                    else:
-                        newIdx = max(0,idx)
-                        # print("newIdx =",newIdx)
-                        try:
-                            idxOfEnd = text.split()[newIdx:].index(args.endTrig)
-                            # print("text =",text)
-                            # print(text.split()[newIdx:])
-                            # print(idxOfEnd)
-                        except ValueError:
-                            idxOfEnd = -1
-                        if (idxOfEnd != -1):
-                            command += " ".join(text.split()[newIdx + (1 if idx >= 0 else 0):idxOfEnd])
-                            print(f"Команда: {command}")
-                            proccessCommand(command)
-                            proccessComm = False
-                            command = ""
-                        else:
-                            command += " ".join(text.split()[newIdx + (1 if idx >= 0 else 0):])
-                            command += " "
-                else:
-                    print("Я всё еще работаю")
-            else:
-                # print(rec.PartialResult())
-                # partial = json.loads(rec.PartialResult())
-                # print(partial.get('partial', ''))
-                pass
 
-except KeyboardInterrupt:
-    print("\nDone")
-    parser.exit(0)
-except Exception as e:
-    parser.exit(type(e).__name__ + ": " + str(e))
+                        # if (args.end_trigger and word == args.end_trigger):
+                        #     command = " ".join(command_words)
+                        #     print("Команда :", command)
+                        #     processCommand(command)
+                        #     listening = False
+                        #     command_words = []
+                        #     last_word_time = None
+                        #     i += 1
+                        #     continue
+
+                        # if not args.end_trigger and last_word_time:
+                        # if last_word_time:
+                        if start - last_word_time > silence:
+                            command = " ".join(command_words)
+                            print("Команда 2:", command)
+                            processCommand(command_words)
+                            listening = False
+                            command_words = []
+                            last_word_time = None
+                            continue
+
+                        command_words.append(word)
+                        last_word_time = end
+                        i += 1
+                    last_result_time = time.perf_counter()
+                else:
+                    if listening:
+                        partial = json.loads(rec.PartialResult())
+                        pText = partial.get('partial','')
+                        pWords = partial.get('partial_result',[])
+                        start = 0.0
+                        if pWords:
+                            start = pWords[0]['start']
+                            end = last_word_time
+                        else:
+                            start = time.perf_counter()
+                            end = last_result_time
+                        if start - end > silence:
+                            command = " ".join(command_words)
+                            print("Команда 3:", command)
+                            processCommand(command_words)
+                            listening = False
+                            command_words = []
+                            last_word_time = None              
+
+    except KeyboardInterrupt:
+        print("\nDone")
+        parser.exit(0)
+    except Exception as e:
+        parser.exit(type(e).__name__ + ": " + str(e))
