95bcd0f61a2ec2b265dfd97ecc7bdc4f6a91fcba
Vitaly Kovganov <dbnfkbqrrr@gmail.com>
2026-04-02 14:21:57 +0300

    upd

--- utils/add_to_json.py
+++ utils/add_to_json.py
@@ -1,6 +1,10 @@
 import json
+import os
 
 def add_element_to_json_list(filename, key, element):
+    if not os.path.isfile(filename):
+        with open(filename, 'w', encoding = "utf-8") as file:
+            file.write("{}")
     with open(filename, 'r', encoding = "utf-8") as file:
         data = json.load(file)
 
